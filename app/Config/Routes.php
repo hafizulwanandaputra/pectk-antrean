de06064f66a4cf7ef6c211b23833a7203a9b985f
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-07-21 10:04:42 +0700

    Add route for list_jaminan in home section

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,6 +14,7 @@ $routes->get('/(?i)logout', 'Auth::logout');
 // HOME
 $routes->get('/(?i)home', 'Home::index');
 $routes->get('/(?i)home/(?i)list_antrean', 'Home::list_antrean');
+$routes->get('/(?i)home/(?i)list_jaminan', 'Home::list_jaminan');
 $routes->get('/(?i)home/(?i)cetak_antrean', 'Home::cetak_antrean');
 
 // ANTREAN

53f9b14a1df0b0487ce6b21dfd958686ee273ac3
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-07-24 09:30:50 +0700

    Remove unused route for calling antrean

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,7 +21,6 @@ $routes->get('/(?i)home/(?i)cetak_antrean/(:num)', 'Home::cetak_antrean/$1');
 $routes->get('/(?i)antrean', 'Antrean::index');
 $routes->get('/(?i)antrean/(?i)list_antrean', 'Antrean::list_antrean');
 $routes->get('/(?i)antrean/(?i)detail_antrean/(:num)', 'Antrean::detail_antrean/$1');
-$routes->post('/(?i)antrean/(?i)panggil_antrean/(:num)', 'Antrean::panggil_antrean/$1');
 $routes->post('/(?i)antrean/(?i)selesai_antrean/(:num)', 'Antrean::selesai_antrean/$1');
 $routes->post('/(?i)antrean/(?i)batal_antrean/(:num)', 'Antrean::batal_antrean/$1');
 
